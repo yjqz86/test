ab33b457b1bc0c8ccf7c9812c576c7d9209b3145
yjqz86 <yeisonquintero@gmail.com>
2015-11-14 21:37:20 -0430

    Commited today

---
+++ PARTES.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="css/estilos.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table width="95%" border="0" align="center" cellpadding="3" cellspacing="0">
+  <tr id="gripTt">
+    <td>Lista de Noticias</td>
+  </tr>
+  <tr>
+    <th align="center" id="grip">NOTICIA</th>
+  </tr>
+  <tr id="grip">
+    <td id="grip"><br />
+        <br />
+      <br /></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="10" background="imagenes/PX/1px_alpha50_ne.png">
+  <tr>
+    <td width="16">&nbsp;</td>
+    <td>sadsadkjhasdkjsda</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<table width="50%" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<table width="50%" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_1.png" width="7" height="7" /></td>
+        <td background="imagenes/PX/1px_alpha50_ne.png"><img src="imagenes/PX/1px_tr.gif" width="7" height="7" /></td>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_2.png" width="7" height="7" /></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_4.png" width="7" height="7" /></td>
+        <td background="imagenes/PX/1px_alpha50_ne.png"><img src="imagenes/PX/1px_tr.gif" width="7" height="7" /></td>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_3.png" width="7" height="7" /></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<table width="50%" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_1.png" width="7" height="7" /></td>
+        <td background="imagenes/PX/1px_alpha50_ne.png"><img src="imagenes/PX/1px_tr.gif" width="7" height="7" /></td>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_2.png" width="7" height="7" /></td>
+      </tr>
+    </table>
+      <table width="100%" border="0" cellspacing="0" cellpadding="5" background="imagenes/PX/1px_alpha50_ne.png">
+        <tr>
+          <td background="imagenes/PX/1px_alpha50_ne.png"><strong>dfsdfsdafsdaf</strong></td>
+        </tr>
+        <tr>
+          <td>jdfh sjdhg dkjlfhg jdfsgh djgh sdkjhg kdljfhg sdjgh sghsdfj gdjgh skjlghdk jfgsdkjgh sdkjgh kfjlg</td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td width="7"><img src="imagenes/BX/cr_desplegable_4.png" width="7" height="7" /></td>
+          <td background="imagenes/PX/1px_alpha50_ne.png"><img src="imagenes/PX/1px_tr.gif" width="7" height="7" /></td>
+          <td width="7"><img src="imagenes/BX/cr_desplegable_3.png" width="7" height="7" /></td>
+        </tr>
+    </table></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+aaa
+<table width="50%" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_alpha25_1.png" width="7" height="7" /></td>
+        <td background="imagenes/PX/1px_alpha25_ne.png"><img src="imagenes/PX/1px_tr.gif" width="7" height="7" /></td>
+        <td width="7"><img src="imagenes/BX/cr_desplegable_alpha25_2.png" width="7" height="7" /></td>
+      </tr>
+    </table>
+      <table width="100%" border="0" cellspacing="0" cellpadding="5" background="imagenes/PX/1px_alpha25_ne.png">
+        <tr>
+          <td background="imagenes/PX/1px_alpha25_ne.png"><strong>Login</strong></td>
+        </tr>
+        <tr>
+          <td align="center"><table width="100" border="0" cellspacing="5" cellpadding="0">
+            <tr>
+              <td colspan="2"><strong>Usuario:</strong><br />
+                <input type="text" name="usuario" id="usuario" /></td>
+            </tr>
+            <tr>
+              <td colspan="2"><strong>Clave:</strong><br />
+                <input type="password" name="clave" id="clave" /></td>
+            </tr>
+            <tr>
+              <td colspan="2">&nbsp;</td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td><input type="submit" name="button" id="button" value="Entrar" /></td>
+            </tr>
+          </table></td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td width="7"><img src="imagenes/BX/cr_desplegable_alpha25_4.png" width="7" height="7" /></td>
+          <td background="imagenes/PX/1px_alpha25_ne.png"><img src="imagenes/PX/1px_tr.gif" width="7" height="7" /></td>
+          <td width="7"><img src="imagenes/BX/cr_desplegable_alpha25_3.png" width="7" height="7" /></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+sss
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
